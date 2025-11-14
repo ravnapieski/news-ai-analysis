2ffdb3b0ebbc9d12fc0e9aa05f5dcabf74f74dfb
ravnapieski <hansapieski@gmail.com>
2025-11-14 07:28:13 +0200

    Fix typo

--- news_ai_analysis/dataset.py
+++ news_ai_analysis/dataset.py
@@ -4,7 +4,7 @@ from loguru import logger
 from tqdm import tqdm
 import typer
 
-from news-ai-analysis.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from news_ai_analysis.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
 app = typer.Typer()
 
